9b915d0a04bcb94859ed13efd8581c8a02cf3cad
john <jwiils1676@gmail.com>
2026-08-25 15:30:58 -0500

    cambiar variables de entorno

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+production: true,
+    putDataAPI: "https://crud-backend-xwqd.onrender.com/",
+    getDataAPI: "https://crud-backend-xwqd.onrender.com/",
+    deleteDataAPI: "https://crud-backend-xwqd.onrender.com/"
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
-    production: true,
-    putDataAPI: "https://crud-backend-xwqd.onrender.com/",
-    getDataAPI: "https://crud-backend-xwqd.onrender.com/",
-    deleteDataAPI: "https://crud-backend-xwqd.onrender.com/"
+    production: false,
+    putDataAPI: "http://localhost:8080/",
+    getDataAPI: "http://localhost:8080/",
+    deleteDataAPI: "http://localhost:8080/"
 
 }
+
